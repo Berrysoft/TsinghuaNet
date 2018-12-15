3c309894a0dbe4a9141fd28cd608da083690e86c
王宇逸 <Strawberry_Str@hotmail.com>
2018-12-16 01:18:41 +0800

    减少头文件包含

--- TsinghuaNetUWP/pch.h
+++ TsinghuaNetUWP/pch.h
@@ -1,12 +1,9 @@
 ﻿#pragma once
 
-#define NOMINMAX
-#include <windows.h>
-
 #include <unknwn.h>
 
 #include <hstring.h>
-#include <restrictederrorinfo.h>
+
 #include <winrt/Windows.ApplicationModel.Activation.h>
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Storage.h>
