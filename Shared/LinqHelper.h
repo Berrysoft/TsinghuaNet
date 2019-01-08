765118a9e41d46c46c50cd14b5271ae7c0991249
王宇逸 <Strawberry_Str@hotmail.com>
2019-01-08 21:45:42 +0800

    提高迭代速度

--- Shared/LinqHelper.h
+++ Shared/LinqHelper.h
@@ -22,17 +22,43 @@ namespace linq
             }
             constexpr decltype(auto) operator*() { return m_iter.Current(); }
         };
+
+        template <typename Collection, typename = std::enable_if_t<winrt::impl::has_GetAt<Collection>::value>>
+        class winrt_fast_container_enumerator
+        {
+        private:
+            Collection m_collection;
+            std::uint32_t m_index, m_size;
+
+        public:
+            constexpr winrt_fast_container_enumerator(Collection const& collection) : m_collection(collection), m_index(0), m_size(m_collection.Size()) {}
+
+            constexpr operator bool() const { return m_index < m_size; }
+            constexpr winrt_fast_container_enumerator& operator++()
+            {
+                ++m_index;
+                return *this;
+            }
+            constexpr decltype(auto) operator*() { return m_collection.GetAt(m_index); }
+        };
     } // namespace impl
 
-    template <typename Collection, typename = decltype(std::declval<Collection>().First())>
-    constexpr auto winrt_enumerable(Collection const& collection)
-    {
-        return enumerable(impl::winrt_container_enumerator<Collection>(collection));
-    }
+    template <typename C, typename = void>
+    constexpr bool has_GetAt_v{ false };
+
+    template <typename C>
+    constexpr bool has_GetAt_v<C, std::void_t<decltype(std::declval<C>().GetAt(0))>>{ true };
 
     template <typename Collection, typename Query, typename = decltype(std::declval<Collection>().First())>
     constexpr auto operator>>(Collection const& c, Query&& q)
     {
-        return std::forward<Query>(q)(winrt_enumerable<Collection>(c));
+        if constexpr (has_GetAt_v<Collection>)
+        {
+            return std::forward<Query>(q)(enumerable(impl::winrt_fast_container_enumerator<Collection>(c)));
+        }
+        else
+        {
+            return std::forward<Query>(q)(enumerable(impl::winrt_container_enumerator<Collection>(c)));
+        }
     }
 } // namespace linq
