d5010c77cdf67cc351bb57975cff6adbbfd5365a
王宇逸 <Strawberry_Str@hotmail.com>
2019-04-21 12:19:29 +0800

    减小体积

--- TsinghuaNetUWP/My Project/AssemblyInfo.vb
+++ TsinghuaNetUWP/My Project/AssemblyInfo.vb
@@ -26,6 +26,6 @@ Imports System.Runtime.InteropServices
 '通过使用 "*"，如下所示:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("3.0.0.0")>
+<Assembly: AssemblyFileVersion("3.0.0.0")>
 <Assembly: ComVisible(False)>
