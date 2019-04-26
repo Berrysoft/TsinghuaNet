fcfba923c0ddb263947d374f490a405548184174
王宇逸 <Strawberry_Str@hotmail.com>
2019-04-26 19:16:42 +0800

    完善明细排序

--- TsinghuaNetUWP/Views/DetailDialog.xaml.vb
+++ TsinghuaNetUWP/Views/DetailDialog.xaml.vb
@@ -13,31 +13,49 @@ Public NotInheritable Class DetailDialog
 
     Private Async Sub InitializeDetails(helper As UseregHelper)
         details = New List(Of NetDetail)(Await helper.GetDetailsAsync())
-        LogoutColumn.SortDirection = DataGridSortDirection.Ascending
-        DetailsView.ItemsSource = New ObservableCollection(Of NetDetail)(details)
+        DetailsView.ItemsSource = details
     End Sub
 
     Private Sub DetailsView_Sorting(sender As Object, e As DataGridColumnEventArgs)
-        If e.Column.SortDirection Is Nothing OrElse e.Column.SortDirection = DataGridSortDirection.Ascending Then
-            e.Column.SortDirection = DataGridSortDirection.Descending
-            Select Case e.Column.Tag
-                Case "LoginTime"
-
-                Case "LogoutTime"
-                    DetailsView.ItemsSource = New ObservableCollection(Of NetDetail)(From d In details Order By d.LogoutTime Descending)
-                Case "Flux"
-
+        If details IsNot Nothing Then
+            Dim dir As DataGridSortDirection?
+            Dim oridir = e.Column.SortDirection
+            For Each c In DetailsView.Columns
+                c.SortDirection = Nothing
+            Next
+            Select Case oridir
+                Case DataGridSortDirection.Ascending
+                    dir = DataGridSortDirection.Descending
+                Case DataGridSortDirection.Descending
+                    dir = Nothing
+                Case Else
+                    dir = DataGridSortDirection.Ascending
             End Select
-        Else
-            e.Column.SortDirection = DataGridSortDirection.Ascending
-            Select Case e.Column.Tag
-                Case "LoginTime"
-
-                Case "LogoutTime"
-                    DetailsView.ItemsSource = New ObservableCollection(Of NetDetail)(From d In details Order By d.LogoutTime Ascending)
-                Case "Flux"
-
-            End Select
+            e.Column.SortDirection = dir
+            If dir IsNot Nothing Then
+                Dim ascending As Boolean = dir.Value = DataGridSortDirection.Ascending
+                Select Case e.Column.Tag
+                    Case "LoginTime"
+                        DetailsView.ItemsSource = details.OrderBy(Function(d) d.LoginTime, ascending)
+                    Case "LogoutTime"
+                        DetailsView.ItemsSource = details.OrderBy(Function(d) d.LogoutTime, ascending)
+                    Case "Flux"
+                        DetailsView.ItemsSource = details.OrderBy(Function(d) d.Flux, ascending)
+                End Select
+            Else
+                DetailsView.ItemsSource = details
+            End If
         End If
     End Sub
 End Class
+
+Module OrderByHelper
+    <Extension>
+    Public Function OrderBy(Of TSource, TKey)(source As IEnumerable(Of TSource), selector As Func(Of TSource, TKey), ascending As Boolean) As IOrderedEnumerable(Of TSource)
+        If ascending Then
+            Return source.OrderBy(selector)
+        Else
+            Return source.OrderByDescending(selector)
+        End If
+    End Function
+End Module
