eb6f25ab03b0037bc198e48a0b438451711d52e1
王宇逸 <Strawberry_Str@hotmail.com>
2019-05-04 15:18:45 +0800

    Avalonia: 实现基本功能

---
+++ TsinghuaNet.Avalonia/NetStateChangeCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TsinghuaNet.Avalonia
+{
+    class NetStateChangeCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => true;
+
+        private MainViewModel model;
+        public NetStateChangeCommand(MainViewModel model) => this.model = model;
+
+        public void Execute(object parameter) => model.State = Enum.Parse<NetState>(parameter.ToString());
+    }
+}
