8c007e273e209baeeaee61b6eabaec40b2d9d99d
王宇逸 <Strawberry_Str@hotmail.com>
2019-05-05 16:40:42 +0800

    Avalonia: 支持Linux上的CoreRT

--- TsinghuaNet.Avalonia/NetStateCheckedConverter.vb
+++ TsinghuaNet.Avalonia/NetStateCheckedConverter.vb
@@ -5,7 +5,7 @@ Class NetStateCheckedConverter
     Implements IValueConverter
 
     Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.Convert
-        Return value = [Enum].Parse(Of NetState)(parameter)
+        Return [Enum].Parse(Of NetState)(parameter) = CType(value, NetState)
     End Function
 
     Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
