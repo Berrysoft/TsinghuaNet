1c183ce526d2a4334577e9b661d4e16792127115
王宇逸 <Strawberry_Str@hotmail.com>
2019-05-06 13:33:38 +0800

    Avalonia: 下线功能

--- TsinghuaNet.Avalonia/Views/ConnectionWindow.xaml.vb
+++ TsinghuaNet.Avalonia/Views/ConnectionWindow.xaml.vb
@@ -12,18 +12,38 @@ Public Class ConnectionWindow
         AttachDevTools()
 #End If
         Program.Selector.EnableThemes(Me)
+        DataContext = Me
     End Sub
 
     Private Sub InitializeComponent()
         AvaloniaXamlLoader.Load(Me)
     End Sub
 
+    Private helper As UseregHelper
+
+    Friend ReadOnly Property ConnectionView As DataGrid
+        Get
+            Return FindControl(Of DataGrid)("ConnectionView")
+        End Get
+    End Property
+
+
     Public Overloads Async Function ShowDialog(owner As Window, model As MainViewModel) As Task Implements IDialogWithModel(Of MainViewModel).ShowDialog
         Dim showtask = ShowDialog(owner)
-        Dim helper = model.GetUseregHelper()
+        helper = model.GetUseregHelper()
         Await helper.LoginAsync()
-        Dim grid = FindControl(Of DataGrid)("ConnectionView")
-        grid.Items = Await helper.GetUsersAsync()
+        Await RefreshUsersAsync()
         Await showtask
     End Function
+
+    Private Async Function RefreshUsersAsync() As Task
+        ConnectionView.Items = Await helper.GetUsersAsync()
+    End Function
+
+    Private Async Sub DropSelection()
+        For Each user As NetUser In ConnectionView.SelectedItems
+            Await helper.LogoutAsync(user.Address)
+        Next
+        Await RefreshUsersAsync()
+    End Sub
 End Class
