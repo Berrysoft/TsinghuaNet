dcd85b948a286b4c3cd77edb4af937acb3ebf529
王宇逸 <Strawberry_Str@hotmail.com>
2019-05-11 23:13:02 +0800

    添加Xamarin项目

---
+++ TsinghuaNet.Xamarin/TsinghuaNet.Xamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TsinghuaNet.Xamarin.Models;
+
+namespace TsinghuaNet.Xamarin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
