7e7ec51236cef2f606ef942b64d634624dfc0d9e
王宇逸 <Strawberry_Str@hotmail.com>
2019-05-16 21:34:32 +0800

    Converters.

--- TsinghuaNet.UWP/Converters/FluxLimitConverter.vb
+++ TsinghuaNet.UWP/Converters/FluxLimitConverter.vb
@@ -1,55 +1,8 @@
-﻿Imports TsinghuaNet.Helper
-
-Public Class NullableLongConverter
+﻿Public Class FluxStringConverter
     Implements IValueConverter
 
     Public Function Convert(value As Object, targetType As Type, parameter As Object, language As String) As Object Implements IValueConverter.Convert
-        Return value IsNot Nothing
-    End Function
-
-    Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, language As String) As Object Implements IValueConverter.ConvertBack
-        If value Then
-            Return 20000000000
-        Else
-            Return Nothing
-        End If
-    End Function
-End Class
-
-Public Class NullableVisibilityConverter
-    Implements IValueConverter
-
-    Public Function Convert(value As Object, targetType As Type, parameter As Object, language As String) As Object Implements IValueConverter.Convert
-        If value Is Nothing Then
-            Return Visibility.Collapsed
-        Else
-            Return Visibility.Visible
-        End If
-    End Function
-
-    Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, language As String) As Object Implements IValueConverter.ConvertBack
-        Throw New NotImplementedException()
-    End Function
-End Class
-
-Public Class FluxNumericConverter
-    Implements IValueConverter
-
-    Public Function Convert(value As Object, targetType As Type, parameter As Object, language As String) As Object Implements IValueConverter.Convert
-        Dim flux As Long = If(value, 20000000000)
-        Return flux / 1000000000
-    End Function
-
-    Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, language As String) As Object Implements IValueConverter.ConvertBack
-        Return CType(CDbl(value) * 1000000000, Long?)
-    End Function
-End Class
-
-Public Class FluxStringConverter
-    Implements IValueConverter
-
-    Public Function Convert(value As Object, targetType As Type, parameter As Object, language As String) As Object Implements IValueConverter.Convert
-        Return StringHelper.GetFluxString(CDbl(value) * 1000000000)
+        Return $"{CDbl(value)} GB"
     End Function
 
     Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, language As String) As Object Implements IValueConverter.ConvertBack
