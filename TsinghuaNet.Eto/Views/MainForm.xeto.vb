55a26249309e92545392ddc637eee79ecb4336f6
王宇逸 <Strawberry_Str@hotmail.com>
2019-05-17 13:20:55 +0800

    Eto: 放弃绑定

--- TsinghuaNet.Eto/Views/MainForm.xeto.vb
+++ TsinghuaNet.Eto/Views/MainForm.xeto.vb
@@ -4,8 +4,17 @@ Imports Eto.Serialization.Xaml
 Public Class MainForm
     Inherits Form
 
+    Private Model As MainViewModel
+
     Public Sub New()
         XamlReader.Load(Me)
+        Model = New MainViewModel()
+        DataContext = Model
+    End Sub
+
+    Private Sub NetStateList_SelectedIndexChanged(sender As Object, e As EventArgs)
+        ' 规避Bug
+        Model.Credential.State = FindChild(Of RadioButtonList)("NetStateList").SelectedIndex + 1
     End Sub
 
 End Class
