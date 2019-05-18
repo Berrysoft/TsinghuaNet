cd4ddc6cf8dc92bf98b4d2623f40803705d7ca33
王宇逸 <Strawberry_Str@hotmail.com>
2019-05-18 11:23:14 +0800

    Eto: 捕获异常

--- TsinghuaNet.Eto/Program.vb
+++ TsinghuaNet.Eto/Program.vb
@@ -4,6 +4,13 @@ Imports Eto.Forms
 Module Program
     Sub Main()
         Dim app As New Application(Platform.Detect)
+#If DEBUG Then
+        AddHandler app.UnhandledException,
+            Sub(sender, e)
+                MessageBox.Show("发生未捕获的异常：" & vbCrLf & e.ExceptionObject.ToString() & vbCrLf & "程序即将退出。", "发生异常", MessageBoxType.Error)
+                Application.Instance.Quit()
+            End Sub
+#End If
         app.Run(New MainForm)
     End Sub
 End Module
