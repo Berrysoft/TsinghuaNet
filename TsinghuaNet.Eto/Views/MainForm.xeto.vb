d87e0b5cba2b517d37798065753c2eaf163548aa
王宇逸 <Strawberry_Str@hotmail.com>
2019-05-20 17:35:10 +0800

    Eto: 使用EnumRadioButtonList

--- TsinghuaNet.Eto/Views/MainForm.xeto.vb
+++ TsinghuaNet.Eto/Views/MainForm.xeto.vb
@@ -1,17 +1,19 @@
-﻿Imports System.ComponentModel
-Imports Eto.Forms
+﻿Imports Eto.Forms
 Imports Eto.Serialization.Xaml
 Imports TsinghuaNet.Helper
 
 Public Class MainForm
     Inherits Form
 
-    Private WithEvents Model As MainViewModel
+    Private Model As MainViewModel
 
     Public Sub New()
         XamlReader.Load(Me)
         Model = New MainViewModel()
         DataContext = Model
+        Dim NetStateList = FindChild(Of EnumRadioButtonList(Of NetState))("NetStateList")
+        AddHandler NetStateList.AddValue, AddressOf NetStateList_AddValue
+        NetStateList.SelectedValueBinding.BindDataContext(Binding.Property(Function(m As MainViewModel) m.Credential.State))
         FindChild(Of Label)("OnlineUserFluxLabel").
             TextBinding.BindDataContext(
                 Binding.Property(Function(m As MainViewModel) m.OnlineUser.Flux).
@@ -22,16 +24,10 @@ Public Class MainForm
                     Convert(AddressOf StringHelper.GetCurrencyString))
     End Sub
 
-    Private Sub NetStateList_SelectedIndexChanged(sender As Object, e As EventArgs)
-        ' 规避Bug
-        Model.Credential.State = FindChild(Of RadioButtonList)("NetStateList").SelectedIndex + 1
-    End Sub
-
-    Private Sub Model_PropertyChanged(sender As Object, e As PropertyChangedEventArgs) Handles Model.PropertyChanged
-        Select Case e.PropertyName
-            Case "SuggestState"
-                FindChild(Of RadioButtonList)("NetStateList").SelectedIndex = Model.SuggestState - 1
-        End Select
+    Private Sub NetStateList_AddValue(sender As Object, e As AddValueEventArgs(Of NetState))
+        If e.Value = NetState.Unknown Then
+            e.ShouldAdd = False
+        End If
     End Sub
 
     Private Sub ShowConnection(sender As Object, e As EventArgs)
