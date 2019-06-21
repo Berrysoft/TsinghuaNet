b6fd5ecd174bb207927d86b24edaf1c8bdaf6f2e
王宇逸 <Strawberry_Str@hotmail.com>
2019-06-22 01:08:18 +0800

    Eto: 修复流量警告

---
+++ TsinghuaNet.Eto/Converters/FluxLimitConverter.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Globalization
+Imports Eto.Forms
+
+Public Class FluxLimitConverter
+    Implements IValueConverter
+
+    Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.Convert
+        Dim bytes As ByteSize = value
+        Return CInt(bytes.GigaBytes)
+    End Function
+
+    Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+        Return ByteSize.FromGigaBytes(value)
+    End Function
+End Class
