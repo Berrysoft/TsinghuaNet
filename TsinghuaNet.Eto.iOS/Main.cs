4df2b26c1364e70dc2af13bae65d65428b1002de
王宇逸 <Strawberry_Str@hotmail.com>
2019-06-22 11:21:18 +0800

    Eto: [WIP]添加iOS

---
+++ TsinghuaNet.Eto.iOS/Main.cs
@@ -0,0 +1,17 @@
+﻿using Eto;
+
+namespace TsinghuaNet.Eto.iOS
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main()
+        {
+            using (App app = new App(Platforms.Ios))
+            using (var form = new MainForm())
+            {
+                app.Run(form);
+            }
+        }
+    }
+}
