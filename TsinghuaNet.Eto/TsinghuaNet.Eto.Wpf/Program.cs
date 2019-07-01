0ddaa2491a40506f64a667cd9b6d2be0c2076c5f
王宇逸 <Strawberry_Str@hotmail.com>
2019-07-01 11:52:27 +0800

    更新SDK

--- TsinghuaNet.Eto/TsinghuaNet.Eto.Wpf/Program.cs
+++ TsinghuaNet.Eto/TsinghuaNet.Eto.Wpf/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Globalization;
 using System.Text;
-using System.Threading;
 using Eto;
 
 namespace TsinghuaNet.Eto.Wpf
@@ -12,13 +10,6 @@ namespace TsinghuaNet.Eto.Wpf
         static void Main()
         {
 #if NETCOREAPP3_0
-            // 规避.NET Core 3.0.0-preview6的bug
-            var culture = CultureInfo.CreateSpecificCulture("en");
-            Thread.CurrentThread.CurrentUICulture = culture;
-            Thread.CurrentThread.CurrentCulture = culture;
-            CultureInfo.DefaultThreadCurrentCulture = culture;
-            CultureInfo.DefaultThreadCurrentUICulture = culture;
-
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 #endif
             using (App app = new App(Platforms.Wpf))
