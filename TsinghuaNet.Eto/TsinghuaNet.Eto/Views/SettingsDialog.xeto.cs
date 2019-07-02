c3aaa13113bfcd52abfa12cc073df7d08588e2c8
王宇逸 <Strawberry_Str@hotmail.com>
2019-07-02 13:05:33 +0800

    Eto: 添加项目地址链接 Uno: 修复链接

--- TsinghuaNet.Eto/TsinghuaNet.Eto/Views/SettingsDialog.xeto.cs
+++ TsinghuaNet.Eto/TsinghuaNet.Eto/Views/SettingsDialog.xeto.cs
@@ -1,4 +1,7 @@
-﻿using Eto.Forms;
+﻿using System;
+using System.Diagnostics;
+using Eto;
+using Eto.Forms;
 using Eto.Serialization.Xaml;
 using TsinghuaNet.Eto.ViewModels;
 
@@ -14,6 +17,24 @@ namespace TsinghuaNet.Eto.Views
             Model = new SettingsViewModel();
             DataContext = Model;
             FindChild<TabControl>("SettingsTab").SelectedIndex = page;
+        }
+
+        private const string RepoUri = "https://github.com/Berrysoft/TsinghuaNet";
+        private void ShowRepo(object sender, EventArgs e)
+        {
+            try
+            {
+                var os = EtoEnvironment.Platform;
+                if (os.IsWindows)
+                    Process.Start(new ProcessStartInfo(RepoUri) { UseShellExecute = true });
+                else if (os.IsLinux)
+                    Process.Start("xdg-open", RepoUri);
+                else if (os.IsMac)
+                    Process.Start("open", RepoUri);
+            }
+            catch (Exception)
+            {
+            }
         }
     }
 }
