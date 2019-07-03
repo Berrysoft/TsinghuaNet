5bf11222a23bdfb1b61f0c7672c6a9df72eb6e97
王宇逸 <Strawberry_Str@hotmail.com>
2019-07-03 23:30:39 +0800

    Eto: 添加按日统计明细

--- TsinghuaNet/Models/DetailViewModel.cs
+++ TsinghuaNet/Models/DetailViewModel.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Windows.Input;
+using TsinghuaNet.Helpers;
 
 namespace TsinghuaNet.Models
 {
     public class DetailViewModel : NetViewModelBase
     {
         public List<NetDetail> InitialDetails { get; set; }
+
+        public List<KeyValuePair<DateTime, ByteSize>> GroupedDetails { get; set; }
 
         public DetailViewModel()
         {
@@ -23,6 +26,8 @@ namespace TsinghuaNet.Models
                 var helper = Credential.GetUseregHelper();
                 await helper.LoginAsync();
                 InitialDetails = await helper.GetDetailsAsync(NetDetailOrder.LogoutTime, false).ToListAsync();
+                DateTime now = DateTime.Now;
+                GroupedDetails = (from d in InitialDetails group d.Flux by d.LogoutTime.Day into g select new KeyValuePair<DateTime, ByteSize>(new DateTime(now.Year, now.Month, g.Key), g.Sum())).ToList();
             }
             catch (Exception ex)
             {
