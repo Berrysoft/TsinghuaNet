6bdd31839a34717f3eb34761905c4f575f316afa
王宇逸 <Strawberry_Str@hotmail.com>
2019-07-14 00:56:51 +0800

    Add Xamarin projects.

---
+++ TsinghuaNet.XF/TsinghuaNet.XF/Views/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace TsinghuaNet.XF.Views
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
