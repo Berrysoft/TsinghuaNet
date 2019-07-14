b6395397d1cc58187bd06f8ebc464acfd583c371
王宇逸 <Strawberry_Str@hotmail.com>
2019-07-14 23:57:54 +0800

    Xamarin: Add Timer.

--- TsinghuaNet.XF/TsinghuaNet.XF/ViewModels/MainViewModel.cs
+++ TsinghuaNet.XF/TsinghuaNet.XF/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using System.Timers;
 using TsinghuaNet.Models;
 using TsinghuaNet.ViewModels;
 using TsinghuaNet.XF.Models;
@@ -9,10 +11,18 @@ namespace TsinghuaNet.XF.ViewModels
 {
     class MainViewModel : MainViewModelBase
     {
+        private readonly Timer mainTimer = new Timer();
+
         public new NetXFSettings Settings
         {
             get => (NetXFSettings)base.Settings;
             set => base.Settings = value;
+        }
+
+        public MainViewModel() : base()
+        {
+            mainTimer.Interval = 1000;
+            mainTimer.Elapsed += MainTimerTick;
         }
 
         public override Task LoadSettingsAsync()
@@ -31,6 +41,33 @@ namespace TsinghuaNet.XF.ViewModels
             Settings.SaveSettings();
             DependencyService.Get<ICredentialStore>().SaveCredential(Credential);
             return Task.CompletedTask;
+        }
+
+        protected override async Task<LogResponse> RefreshAsync(IConnect helper)
+        {
+            var res = await base.RefreshAsync(helper);
+            mainTimer.Stop();
+            // 更新磁贴
+            //NotificationHelper.UpdateTile(OnlineUser);
+            if (!string.IsNullOrEmpty(OnlineUser.Username))
+                mainTimer.Start();
+            if (Settings.EnableFluxLimit && OnlineUser.Flux > Settings.FluxLimit)
+                res = new LogResponse(false, $"流量已使用超过{Settings.FluxLimit}");
+            // 设置内容
+            OnlineTime = OnlineUser.OnlineTime;
+            //if (Window.Current.Content is Frame rootFrame)
+            //{
+            //    if (rootFrame.Content is MainPage mainPage)
+            //    {
+            //        mainPage.BeginFluxAnimation();
+            //    }
+            //}
+            return res;
+        }
+
+        private void MainTimerTick(object sender, ElapsedEventArgs e)
+        {
+            OnlineTime += TimeSpan.FromSeconds(1);
         }
     }
 }
