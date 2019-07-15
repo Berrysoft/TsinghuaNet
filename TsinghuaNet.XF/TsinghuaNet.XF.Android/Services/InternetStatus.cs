d8d380d225d2ac79d4aba3aa42f55149a8b6c59f
王宇逸 <Strawberry_Str@hotmail.com>
2019-07-16 01:26:21 +0800

    Xamarin.Android: 添加InternetStatus

---
+++ TsinghuaNet.XF/TsinghuaNet.XF.Android/Services/InternetStatus.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Net.Wifi;
+using TsinghuaNet.Models;
+using TsinghuaNet.XF.Droid.Services;
+
+[assembly: Xamarin.Forms.Dependency(typeof(InternetStatus))]
+
+namespace TsinghuaNet.XF.Droid.Services
+{
+    public class InternetStatus : NetMapStatus
+    {
+        public override Task RefreshAsync()
+        {
+            WifiManager wifiManager = (WifiManager)Application.Context.GetSystemService(Context.WifiService);
+            if (wifiManager != null)
+            {
+                Status = NetStatus.Wlan;
+                Ssid = wifiManager.ConnectionInfo.SSID.Trim('\"');
+            }
+            else
+            {
+                Status = NetStatus.Unknown;
+                Ssid = string.Empty;
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ TsinghuaNet.XF/TsinghuaNet.XF/Converters/EnumInt32Converter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace TsinghuaNet.XF.Converters
+{
+    class EnumInt32Converter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => Enum.GetValues(targetType).GetValue((int)value);
+    }
+}
