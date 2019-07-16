6a84b176fff1a9a2d62c8ee3a4a7fcfc892ac7fe
王宇逸 <Strawberry_Str@hotmail.com>
2019-07-16 15:09:54 +0800

    Xamarin: Nit

--- TsinghuaNet.XF/TsinghuaNet.XF/Converters/NetStateInt32Converter.cs
+++ TsinghuaNet.XF/TsinghuaNet.XF/Converters/NetStateInt32Converter.cs
@@ -4,10 +4,10 @@ using Xamarin.Forms;
 
 namespace TsinghuaNet.XF.Converters
 {
-    class EnumInt32Converter : IValueConverter
+    class NetStateInt32Converter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value;
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => Enum.GetValues(targetType).GetValue((int)value);
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => (NetState)(int)value;
     }
 }
