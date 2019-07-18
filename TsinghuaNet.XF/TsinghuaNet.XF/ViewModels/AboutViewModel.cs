006450df8cecc4d73076717c674120aa83ec6685
王宇逸 <Strawberry_Str@hotmail.com>
2019-07-19 01:00:18 +0800

    Xamarin: 补充第三方开源库

--- TsinghuaNet.XF/TsinghuaNet.XF/ViewModels/AboutViewModel.cs
+++ TsinghuaNet.XF/TsinghuaNet.XF/ViewModels/AboutViewModel.cs
@@ -3,20 +3,32 @@ using System.Collections.Generic;
 using System.Reflection;
 using TsinghuaNet.Models;
 using TsinghuaNet.ViewModels;
+using Xamarin.Forms;
 
 namespace TsinghuaNet.XF.ViewModels
 {
     class AboutViewModel : NetViewModelBase
     {
         public Version Version => Assembly.GetExecutingAssembly().GetName().Version;
-        public IEnumerable<PackageBox> Packages { get; } = new List<PackageBox>
+        public List<PackageBox> Packages { get; } = new List<PackageBox>
         {
             new PackageBox("Fody", "MIT"),
             new PackageBox("HtmlAgilityPack", "MIT"),
             new PackageBox("PropertyChanged.Fody", "MIT"),
             new PackageBox("Rg.Plugins.Popup", "MIT"),
             new PackageBox("System.Linq.Async","Apache-2.0"),
+            new PackageBox("Xam.Plugin.Connectivity", "MIT"),
             new PackageBox("Xamarin.Forms.DataGrid", "MIT")
         };
+
+        public AboutViewModel() : base()
+        {
+            switch (Device.RuntimePlatform)
+            {
+                case Device.UWP:
+                    Packages.Add(new PackageBox("Microsoft.Toolkit.Uwp", "MIT"));
+                    break;
+            }
+        }
     }
 }
