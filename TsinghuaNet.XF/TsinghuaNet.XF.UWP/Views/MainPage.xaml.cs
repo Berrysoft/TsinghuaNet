1096c671fd0fb0da994e827a412d78bb079512ab
王宇逸 <Strawberry_Str@hotmail.com>
2019-07-19 00:35:31 +0800

    Xamarin: 调整颜色

--- TsinghuaNet.XF/TsinghuaNet.XF.UWP/Views/MainPage.xaml.cs
+++ TsinghuaNet.XF/TsinghuaNet.XF.UWP/Views/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms.Platform.UWP;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.DataGrid;
+using Xamarin.Forms.Platform.UWP;
 
 namespace TsinghuaNet.XF.UWP.Views
 {
@@ -7,7 +9,20 @@ namespace TsinghuaNet.XF.UWP.Views
         public MainPage()
         {
             InitializeComponent();
-            LoadApplication(new TsinghuaNet.XF.App());
+            var xfApp = new TsinghuaNet.XF.App();
+            SetDataGridForeground(xfApp);
+            LoadApplication(xfApp);
+        }
+
+        private void WindowsPage_ActualThemeChanged(Windows.UI.Xaml.FrameworkElement sender, object args)
+        {
+            SetDataGridForeground(Application.Current);
+        }
+
+        private void SetDataGridForeground(Application app)
+        {
+            ((PaletteCollection)app.Resources["DataGridBackgroundPalette"])[0] = ActualTheme == Windows.UI.Xaml.ElementTheme.Dark ? Color.Black : Color.White;
+            ((PaletteCollection)app.Resources["DataGridForegroundPalette"])[0] = ActualTheme == Windows.UI.Xaml.ElementTheme.Dark ? Color.White : Color.Black;
         }
     }
 }
