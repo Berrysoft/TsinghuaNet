b2f63f93510733e1fc2bd3baa67af9d5d93cc186
王宇逸 <Strawberry_Str@hotmail.com>
2019-07-20 20:47:35 +0800

    Eto: 第三方库

--- TsinghuaNet.Eto/TsinghuaNet.Eto/ViewModels/SettingsViewModel.cs
+++ TsinghuaNet.Eto/TsinghuaNet.Eto/ViewModels/SettingsViewModel.cs
@@ -21,30 +21,35 @@ namespace TsinghuaNet.Eto.ViewModels
         public List<PackageBox> Packages { get; } = new List<PackageBox>()
         {
             new PackageBox("Eto.Forms", "BSD-3"),
+            new PackageBox("Eto.Forms.Controls.SkiaSharp","MIT"),
             new PackageBox("Eto.Serialization.Xaml", "BSD-3"),
             new PackageBox("Fody", "MIT"),
             new PackageBox("HtmlAgilityPack", "MIT"),
             new PackageBox("Portable.Xaml", "MIT"),
             new PackageBox("PropertyChanged.Fody", "MIT"),
             new PackageBox("Refractored.MvvmHelpers", "MIT"),
+            new PackageBox("SkiaSharp","MIT"),
             new PackageBox("System.Linq.Async","Apache-2.0")
         };
 
-        public Version Version { get => Assembly.GetExecutingAssembly().GetName().Version; }
+        public Version Version => Assembly.GetExecutingAssembly().GetName().Version;
 
         public SettingsViewModel() : base()
         {
             var platform = Platform.Instance;
             if (platform.IsWpf)
+            {
                 Packages.Add(new PackageBox("Eto.Platform.Wpf", "BSD-3"));
+            }
             else if (platform.IsGtk)
             {
                 Packages.Add(new PackageBox("Eto.Platform.Gtk", "BSD-3"));
                 Packages.Add(new PackageBox("GtkSharp", "LGPLv2"));
             }
             else if (platform.IsMac)
+            {
                 Packages.Add(new PackageBox("Eto.Platform.Mac64", "BSD-3"));
-            Packages.Sort((PackageBox p1, PackageBox p2) => p1.Name.CompareTo(p2.Name));
+            }
         }
     }
 }
