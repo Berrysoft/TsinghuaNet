ff4e3b80dc898b207e2494a5496bb43dc69782ed
王宇逸 <Strawberry_Str@hotmail.com>
2019-09-06 13:30:21 +0800

    Xamarin.UWP: 修复Frame颜色

---
+++ TsinghuaNet.XF/TsinghuaNet.XF.UWP/Renderers/FrameAcrylicRenderer.cs
@@ -0,0 +1,19 @@
+﻿using TsinghuaNet.XF.UWP.Renderers;
+using Windows.UI.Xaml.Media;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(Frame), typeof(FrameAcrylicRenderer))]
+
+namespace TsinghuaNet.XF.UWP.Renderers
+{
+    public class FrameAcrylicRenderer : FrameRenderer
+    {
+        protected override void UpdateBackgroundColor()
+        {
+            base.UpdateBackgroundColor();
+            if (Control != null)
+                Control.Background = (Brush)Windows.UI.Xaml.Application.Current.Resources["SystemControlAcrylicElementBrush"];
+        }
+    }
+}
--- TsinghuaNet.XF/TsinghuaNet.XF.UWP/Views/MainPage.xaml.cs
+++ TsinghuaNet.XF/TsinghuaNet.XF.UWP/Views/MainPage.xaml.cs
@@ -23,9 +23,7 @@ namespace TsinghuaNet.XF.UWP.Views
         private void SetColorResource(xf.Application app)
         {
             var foreground = ActualTheme == ElementTheme.Dark ? xf.Color.White : xf.Color.Black;
-            var background = ActualTheme == ElementTheme.Dark ? xf.Color.Black : xf.Color.White;
             ((PaletteCollection)app.Resources["DataGridForegroundPalette"])[0] = foreground;
-            app.Resources["DefaultPageBackground"] = background;
         }
     }
 }
