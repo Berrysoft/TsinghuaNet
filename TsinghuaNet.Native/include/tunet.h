4d82fe8abb00932cd7da1b4064c7137db27c82c8
Berrysoft <Strawberry_Str@hotmail.com>
2019-11-05 13:12:25 +0800

    Native: 修复C头文件

--- TsinghuaNet.Native/include/tunet.h
+++ TsinghuaNet.Native/include/tunet.h
@@ -4,12 +4,20 @@
 #ifndef TUNET_API
 #ifdef _MSC_VER
 #define TUNET_API __cdecl
-#elif defined(__GNUC__)
-#define TUNET_API __attribute__((__cdecl__))
 #else
 #define TUNET_API
 #endif
 #endif // !TUNET_API
+
+#ifndef TUNET_RESTRICT
+#if defined(_MSC_VER)
+#define TUNET_RESTRICT __restrict
+#elif defined(__GNUC__)
+#define TUNET_RESTRICT __restrict__
+#else
+#define TUNET_RESTRICT
+#endif
+#endif // !TUNET_RESTRICT
 
 #ifdef __cplusplus
 extern "C"
@@ -68,18 +76,18 @@ extern "C"
 
     int32_t TUNET_API tunet_login(const tunet_credential* cred);
     int32_t TUNET_API tunet_logout(const tunet_credential* cred);
-    int32_t TUNET_API tunet_status(const tunet_credential* cred, tunet_flux* flux);
+    int32_t TUNET_API tunet_status(const tunet_credential* TUNET_RESTRICT cred, tunet_flux* TUNET_RESTRICT flux);
 
     int32_t TUNET_API tunet_usereg_login(const tunet_credential* cred);
     int32_t TUNET_API tunet_usereg_logout(const tunet_credential* cred);
     int32_t TUNET_API tunet_usereg_drop(const tunet_credential* cred, int64_t addr);
 
     int32_t TUNET_API tunet_usereg_users(const tunet_credential* cred);
-    int32_t TUNET_API tunet_usereg_users_destory();
+    int32_t TUNET_API tunet_usereg_users_destory(void);
     int32_t TUNET_API tunet_usereg_users_fetch(int32_t index, tunet_user* user);
 
     int32_t TUNET_API tunet_usereg_details(const tunet_credential* cred, tunet_detail_order order, int descending);
-    int32_t TUNET_API tunet_usereg_details_destory();
+    int32_t TUNET_API tunet_usereg_details_destory(void);
     int32_t TUNET_API tunet_usereg_details_fetch(int32_t index, tunet_detail* detail);
 
 #ifdef __cplusplus
