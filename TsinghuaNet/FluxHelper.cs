8c261bd5a7d8629f9906801a5282b6cf855efef3
Berrysoft <Strawberry_Str@hotmail.com>
2020-01-01 00:13:25 +0800

    更新剩余流量算法

--- TsinghuaNet/FluxHelper.cs
+++ TsinghuaNet/FluxHelper.cs
@@ -5,9 +5,9 @@ namespace TsinghuaNet
 {
     public static class FluxHelper
     {
-        public static readonly ByteSize BaseFlux = ByteSize.FromGigaBytes(25);
+        public static readonly ByteSize BaseFlux = ByteSize.FromGigaBytes(50);
 
         public static ByteSize GetMaxFlux(ByteSize flux, decimal balance)
-        => ByteSize.FromBytes(Math.Max(flux.Bytes, BaseFlux.Bytes)) + ByteSize.FromGigaBytes((double)balance / 2);
+        => ByteSize.FromBytes(Math.Max(flux.Bytes, BaseFlux.Bytes)) + ByteSize.FromGigaBytes((double)balance);
     }
 }
